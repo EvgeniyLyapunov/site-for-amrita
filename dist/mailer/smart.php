69d3b353b2b31a441536a632a7daca8a1861bb99
EvgeniyLyapunov <lyapunovcs@gmail.com>
2022-08-30 23:31:06 +0300

    added option make order via modal window and email

--- dist/mailer/smart.php
+++ dist/mailer/smart.php
@@ -1,8 +1,8 @@
 <?php 
 
 $name = $_POST['name'];
-$phone = $_POST['phone'];
-$email = $_POST['email'];
+$contact = $_POST['contact'];
+$message = $_POST['message'];
 
 require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
@@ -13,13 +13,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'little-wing@list.ru';                 // Наш логин
-$mail->Password = 'ZqEYz7TxzXyptL5tZmLm';                           // Наш пароль от ящика
+$mail->Username = 'anitasaluja@mail.ru';                 // Наш логин
+$mail->Password = 'Es7dFDepte9jRccYH2YA';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('little-wing@list.ru', 'Pulse');   // От кого письмо 
-$mail->addAddress('main@little-wing.ru');     // Add a recipient
+$mail->setFrom('anitasaluja@mail.ru', 'MySite');   // От кого письмо 
+$mail->addAddress('anitasaluja@yandex.ru');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
@@ -32,8 +32,8 @@ $mail->Subject = 'Данные';
 $mail->Body    = '
 		Пользователь оставил данные <br> 
 	Имя: ' . $name . ' <br>
-	Номер телефона: ' . $phone . '<br>
-	E-mail: ' . $email . '';
+	Как связаться: ' . $contact . '<br>
+	Сообщение: ' . $message . '';
 
 if(!$mail->send()) {
     return false;
--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,80 @@ $(document).ready(function(){
     slidesToShow: 1,
     centerMode: true,
     variableWidth: true,
+    // dots: true,
+    // infinite: true,
+    // speed: 500,
+    // fade: true,
+    // cssEase: 'linear',
     prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.svg"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.svg"></button>'
   });
+
+   // smooth scroll and pageup
+   $(window).scroll(function() {
+    if($(this).scrollTop() > 1600) {
+      $(".pageup").fadeIn();
+    }
+    else {
+      $(".pageup").fadeOut();
+    }
+  });
+
+    // modal windows
+    $("[data-modal=order-pic]").on("click", function() {
+      $(".overlay, #order").fadeIn("slow");
+    });
+    $(".modal__close").on("click", function() {
+      $("form").trigger("reset");
+      $(".overlay, #order, #thanks").fadeOut("slow");
+    });
+
+    function valideForms(form) {
+      $(form).validate({
+        rules: {
+          name: {
+            required: true,
+            minlength: 2
+          },
+          contact: {
+            required: true,
+            minlength: 2
+          }
+        },
+        messages: {
+          name: {
+            required: "Пожалуйста, представьтесь",
+            minlength: jQuery.validator.format("Введите минимум {0} символа!")
+          },
+          contact: {
+            required: "Пожалуйста, как с вами связаться?",
+            minlength: jQuery.validator.format("Не реально короткий контакт!")
+          }
+        }
+      });
+    };
+  
+    valideForms("#order-form");
+  
+
+    $("form").submit(function(e) {
+      e.preventDefault();
+  
+
+      $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+      }).done(function() {
+        $(this).find("input").val("");
+        $(this).find("textaria").val("");
+
+        $("#order").fadeOut();
+        $(".overlay, #thanks").fadeIn("slow");
+  
+        $("form").trigger("reset");
+      });
+      return false;
+    });
 });
+
